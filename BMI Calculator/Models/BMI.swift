0e584efa38f2a524546ef940f9603bd9fbb0b531
Oleksandr Oliinyk <alex.oliinyk@gmail.com>
2021-07-20 16:00:56 +0300

    update

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Alex Oliinyk on 18.07.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
